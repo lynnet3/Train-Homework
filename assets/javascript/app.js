1b8504d7176fb2064f1e0eff5f89453a4e7d9619
Emily Tucker <emilytucker4@yahoo.com>
2019-08-27 19:06:17 -0400

    added my firebase js and the database

---
+++ assets/javascript/app.js
@@ -0,0 +1,14 @@
+$(document), ready(function () {
+    // Your web app's Firebase configuration
+    var config = {
+        apiKey: "AIzaSyCC8qE0LPCxN9sgQsw1eIWiI0PzWrCm54M",
+        authDomain: "train-time-sheet-hw.firebaseapp.com",
+        databaseURL: "https://train-time-sheet-hw.firebaseio.com",
+        projectId: "train-time-sheet-hw",
+        storageBucket: "",
+        messagingSenderId: "585815854157",
+        appId: "1:585815854157:web:3ae435faee55d4d1"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(config);
+})
